35ff1781d5d6c55e2ac5e4f16cc31f2a79448148
Bibek Aryal <34973169+bibekaryal86@users.noreply.github.com>
2026-07-07 19:41:15 -0600

    Remove Unused Variable (#54)

--- app/src/main/java/budget/application/server/core/ServerSecurity.java
+++ app/src/main/java/budget/application/server/core/ServerSecurity.java
@@ -18,12 +18,11 @@ public class ServerSecurity extends ChannelDuplexHandler {
   public void channelRead(final ChannelHandlerContext channelHandlerContext, final Object object)
       throws Exception {
     if (object instanceof FullHttpRequest fullHttpRequest) {
-      final String requestId = channelHandlerContext.channel().attr(Constants.REQUEST_ID).get();
       final String requestUri = fullHttpRequest.uri();
 
       final boolean isNoAuth = isNoAuthCheck(requestUri);
       if (isNoAuth) {
-        log.debug("[{}] No Auth Request...", requestId);
+        log.debug("No Auth Request: [{}]", requestUri);
         super.channelRead(channelHandlerContext, fullHttpRequest);
         return;
       }
@@ -34,16 +33,13 @@ public class ServerSecurity extends ChannelDuplexHandler {
       }
 
       if (CommonUtilities.isEmpty(authHeader)) {
-        ResponseWithMetadata response =
-            ServerUtils.getResponseWithMetadata(
-                String.format("[%s] Not Authenticated...", requestId));
+        ResponseWithMetadata response = ServerUtils.getResponseWithMetadata("Not Authenticated...");
         ServerUtils.sendResponse(channelHandlerContext, HttpResponseStatus.UNAUTHORIZED, response);
         return;
       }
 
       if (!isBasicAuthenticated(authHeader)) {
-        ResponseWithMetadata response =
-            ServerUtils.getResponseWithMetadata(String.format("[%s] Not Authorized...", requestId));
+        ResponseWithMetadata response = ServerUtils.getResponseWithMetadata("Not Authorized...");
         ServerUtils.sendResponse(channelHandlerContext, HttpResponseStatus.UNAUTHORIZED, response);
         return;
       }
